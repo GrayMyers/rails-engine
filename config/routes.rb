35004d78c518cb63d17078bf0f514c9c534c9935
Grayson Myers <graymyers10@gmail.com>
2021-02-18 10:10:03 -0700

    Add routing for restful requests

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      resources :items, only: [:index, :show, :create, :update, :destroy] do
+        get "/merchant", to: "items#merchant"
+      end
+
       resources :merchants, only: [:index, :show] do
-        resources :items
+        get "/items", to: "merchants#items"
       end
     end
   end
