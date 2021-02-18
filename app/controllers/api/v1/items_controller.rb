96d3234d7c72fa783a592a6cba95ea8385a31010
Grayson Myers <graymyers10@gmail.com>
2021-02-18 10:10:59 -0700

    Add item restful endpoints

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,8 +1,52 @@
 class Api::V1::ItemsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
 
   def index
-    merchant = Merchant.find(params[:merchant_id].to_i)
-    items = ItemSerializer.new(merchant.items)
-    render json: items
+    render json: ItemSerializer.new(Item.all_within_range(params[:page].to_i,params[:per_page].to_i))
+  end
+
+  def show
+    render json: ItemSerializer.new(Item.find(params[:id].to_i))
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: ItemSerializer.new(item), status: 201
+
+    else
+      render json: {:error => "invalid data", status: 400}.to_json, status: 400
+    end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      render json: {status: 204}.to_json, status: 204
+    else
+      render json: {:error => "invalid data", status: 400}.to_json, status: 400
+    end
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      render json: ItemSerializer.new(item), status: 201
+    else
+      render json: {:error => "invalid data", status: 400}.to_json, status: 400
+    end
+  end
+
+  def merchant
+    item = Item.find(params[:item_id])
+    merchant = item.merchant
+    render json: MerchantSerializer.new(merchant)
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :unit_price, :merchant_id)
   end
 end
