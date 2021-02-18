49f37b0a473c2737860adac5278a2e0ccc2c04f1
Grayson Myers <graymyers10@gmail.com>
2021-02-18 15:10:57 -0700

    Add test for find_all method of item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+
+  def find_all(search_term,min,max)
+    Item
+    .where("regexp_matches(name, ?)", "#{search}") if search_term
+    .where("price > #{min}") if min
+    .where("price < #{max}") if max
+  end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,7 @@
 class Merchant < ApplicationRecord
   has_many :items
+
+  def self.find_one(search_term)
+    Merchant.all.where("name LIKE ?","%#{search_term}%").limit(1)[0]
+  end
 end
