44344136cc08d2ef88f0b3dcfbde6d87eaa669bc
Grayson Myers <graymyers10@gmail.com>
2021-02-18 10:11:41 -0700

    Add 404 error handling in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   private
 
   def render_404
-    render json: {:error => "data not found!", :status => 404}.to_json, :status => 404
+    render json: {:error => "the resource could not be located", :status => 404}.to_json, :status => 404
   end
 end
