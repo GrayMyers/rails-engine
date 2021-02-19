27286f02d9a85211406b639e0cd8f03159846266
Grayson Myers <graymyers10@gmail.com>
2021-02-18 17:54:21 -0700

    Add total_revenue_within_range method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,4 +5,16 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
 
+  def self.total_revenue_within_range(start_date,end_date)
+    invoices = Invoice.all
+
+    invoices = invoices.where("invoices.created_at > ?",start_date) if start_date
+
+    invoices = invoices.where("invoices.created_at < ?",end_date) if end_date
+
+    invoices.joins(:invoice_items, :transactions).
+    where("invoices.status = 'shipped' AND transactions.result = 'success'").
+    select("invoice_items.unit_price, invoice_items.quantity").
+    sum("invoice_items.unit_price * invoice_items.quantity")
+  end
 end
