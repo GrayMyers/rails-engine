616e7b3e503c3ee7183d6470ca35825c71672e60
Grayson Myers <graymyers10@gmail.com>
2021-02-18 18:42:37 -0700

    Add routing for pending revenue request

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       get "/merchants/find_one", to: "search#find_one_merchant"
       get "/items/find_all", to: "search#find_all_items"
+
       get "/market/total_revenue_within_range", to: "business_intelligence#total_revenue_within_range"
+      get "/market/pending_revenue", to: "business_intelligence#pending_revenue"
 
 
       resources :items, only: [:index, :show, :create, :update, :destroy] do
