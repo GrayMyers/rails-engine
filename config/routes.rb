2247b59c81fb3767f59e37cd32e0fac81590d7b5
Grayson Myers <graymyers10@gmail.com>
2021-02-18 18:14:41 -0700

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       get "/merchants/find_one", to: "search#find_one_merchant"
       get "/items/find_all", to: "search#find_all_items"
+      get "/market/total_revenue_within_range", to: "business_intelligence#total_revenue_within_range"
+
 
       resources :items, only: [:index, :show, :create, :update, :destroy] do
         get "/merchant", to: "items#merchant"
